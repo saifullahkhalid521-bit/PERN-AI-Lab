7da4f4b005901e6a76447633816a9da941868322
Saifullah Khalid <saifullahkhalid521@gmail.com>
2026-09-07 19:53:51 +0530

    fetch() POST request

--- Phase_1_JS_&_git/Week-3/fetch.js
+++ Phase_1_JS_&_git/Week-3/fetch.js
@@ -37,16 +37,76 @@ fetch('https://v2.jokeapi.dev/joke/Any?type=single')
     });
 
 
-fetch('https://jsonplaceholder.typicode.com/users')
-.then(response =>{
-  if(!response.ok){
-    throw new Error ('Error hai mari jaan' , response.status);
-  }
-  return response.json();
+// fetch('https://jsonplaceholder.typicode.com/users')
+// .then(response =>{
+//   if(!response.ok){
+//     throw new Error ('Error hai mari jaan' , response.status);
+//   }
+//   return response.json();
+// })
+// .then(data =>{
+//   console.log(data);
+// })
+// .catch(err =>{
+//   console.log(err);
+// })
+
+
+// POST request
+const userData = {
+  name : 'Mr.Robot',
+  suj : 'Hacking',
+  id : 101,
+}
+
+fetch('https://jsonplaceholder.typicode.com/users' , {
+  method: 'POST',
+  headers:{
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(userData)
 })
-.then(data =>{
+.then((res)=>res.json())
+.then((data)=>console.log(data))
+.catch((err)=>console.log(err));
+
+
+const newData2 = {
+  title: 'My Post',
+  body: 'Content here',
+  userId: 1,
+}
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  headers:{
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(newData2)
+})
+.then(res=>res.json())
+.then(data=>console.log(data))
+.catch(err=>console.log(err));
+
+
+const newData3 ={
+  name : 'Ego',
+  age : 'Infinity',
+  job : 'fuck you',
+}
+fetch('https://jsonplaceholder.typicode.com/posts' , {
+  method: 'POST',
+  headers:{
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(newData3)
+})
+.then((resp)=>{
+  return resp.json();
+})
+.then((data)=>{
   console.log(data);
 })
-.catch(err =>{
+.catch((err)=>{
   console.log(err);
-})
+});
