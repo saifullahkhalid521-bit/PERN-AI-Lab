c5569902509d0ed69ad3bef04945240617d396ea
Saifullah Khalid <saifullahkhalid521@gmail.com>
2026-09-09 20:45:56 +0530

    promise chaning with fetch

--- Phase_1_JS_&_git/Week-3/day1.js
+++ Phase_1_JS_&_git/Week-3/day1.js
@@ -1,69 +1,77 @@
-console.log("1. I start learing JavaScript");
+// console.log("1. I start learing JavaScript");
 
-setTimeout(()=> {
-  console.log('3. Time task completed.');
-},2000);
+// setTimeout(()=> {
+//   console.log('3. Time task completed.');
+// },2000);
 
-setTimeout(()=> {
-  console.log('4. Main bhi turant chlta hoon.');
-}, 2000);
+// setTimeout(()=> {
+//   console.log('4. Main bhi turant chlta hoon.');
+// }, 2000);
 
-console.log('2. I do other work while waiting.');
+// console.log('2. I do other work while waiting.');
 
 
 
-new Promise ((resolve , reject) => {
-  setTimeout(()=>{
-    let num = 23;
-    if (num === 23){
-      const person = {
-        id: 101,
-        name: 'Mr.Robot'
-      }
-      resolve( person);
-    }
-    else{
-      reject("Variable did't match!");
-    }
-  },2000);
-}).then((response)=>{
-  console.log(response);
-  // console.log(response.name);
-  return response.name;
-}).then((upData)=>{
-  console.log(`nicha ka then hai lala: ${upData}`);
+// new Promise ((resolve , reject) => {
+//   setTimeout(()=>{
+//     let num = 23;
+//     if (num === 23){
+//       const person = {
+//         id: 101,
+//         name: 'Mr.Robot'
+//       }
+//       resolve( person);
+//     }
+//     else{
+//       reject("Variable did't match!");
+//     }
+//   },2000);
+// }).then((response)=>{
+//   console.log(response);
+//   // console.log(response.name);
+//   return response.name;
+// }).then((upData)=>{
+//   console.log(`nicha ka then hai lala: ${upData}`);
+// })
+// .catch((err)=>{
+//   console.log("ERROR: ", err);
+// }).finally(()=>{
+//   console.log("Ho gaya finally!");
+// });
+
+// function AAJA (){
+//   return new Promise ((resolve , reject)=>{
+//     setTimeout(()=>{
+//       let error = false;
+//       if(!error){
+//         const user = {
+//           roll: 65,
+//           sub: "JABHA_SCRIPT"
+//         }
+//         resolve(user);
+//       }
+//       else{
+//         reject("KAHA GAYA MERA JABHA😢");
+//       }
+//     },2000);
+//   })
+// }
+
+// async function JAABHA () {
+//   try {
+//     const le = await AAJA();
+//     console.log("YESSS😍 aa gaya mera",le.sub);
+//   } catch (err) {
+//       console.log(`ERROR: ${err}`);
+//   }
+// }
+
+// JAABHA();
+
+fetch( `https://jsonplaceholder.typicode.com/comments?postId=4`)
+.then((response)=>{
+  return response.json();
 })
-.catch((err)=>{
-  console.log("ERROR: ", err);
-}).finally(()=>{
-  console.log("Ho gaya finally!");
-});
-
-function AAJA (){
-  return new Promise ((resolve , reject)=>{
-    setTimeout(()=>{
-      let error = false;
-      if(!error){
-        const user = {
-          roll: 65,
-          sub: "JABHA_SCRIPT"
-        }
-        resolve(user);
-      }
-      else{
-        reject("KAHA GAYA MERA JABHA😢");
-      }
-    },2000);
-  })
-}
-
-async function JABHA () {
-  try {
-    const le = await AAJA();
-    console.log("YESSS😍 aa gaya mera",le.sub);
-  } catch (err) {
-      console.log(`ERROR: ${err}`);
-  }
-}
-
-JABHA();
+.then((data)=>{
+  console.log(data);
+})
--- Phase_1_JS_&_git/Week-3/promises.js
+++ Phase_1_JS_&_git/Week-3/promises.js
@@ -1,110 +1,110 @@
-// const promiseOne = new Promise(function (resolve , reject){
-//   setTimeout(()=>{
-//     resolve();
-//     console.log("Async task is compelete");
-//   },1000);
-// })
+const promiseOne = new Promise(function (resolve , reject){
+  setTimeout(()=>{
+    resolve();
+    console.log("Async task is compelete");
+  },1000);
+})
 
-// promiseOne.then(()=>{
-//   console.log("PromisOne resolve done!");
-// })
+promiseOne.then(()=>{
+  console.log("PromisOne resolve done!");
+})
 
-// new Promise((resolve , reject)=>{
-//   setTimeout(()=>{
-//     resolve();
-//     console.log("Second promise is done.");
-//   },1000);
-// }).then(()=>{
-//   console.log('Second resolve is also done!');
-// })
-
-
-// const promiseThree = new Promise((resolve , reject)=>{
-//   setTimeout(()=>{
-//     resolve({id: 101 , name:"Robot"});
-//     console.log("Thired promise one the way.");
-//   },1000);
-// })
-
-// promiseThree.then((user)=>{
-//   console.log("Thired promise is completed!");
-//   console.log(`User id is: ${user.id} and user name is: ${user.name}`);
-// });
+new Promise((resolve , reject)=>{
+  setTimeout(()=>{
+    resolve();
+    console.log("Second promise is done.");
+  },1000);
+}).then(()=>{
+  console.log('Second resolve is also done!');
+})
 
 
-// const promiseFour = new Promise ((resolve , reject)=>{
-//   setTimeout(()=>{
-//     let error = false;
-//     if(!error){
-//       resolve({username: "Robot" , password: 1234});
-//     }
-//     else{
-//       reject("ERROR: something went wrong.");
-//     }
-//   },1000)
-// })
+const promiseThree = new Promise((resolve , reject)=>{
+  setTimeout(()=>{
+    resolve({id: 101 , name:"Robot"});
+    console.log("Thired promise one the way.");
+  },1000);
+})
 
-// promiseFour
-// .then((data)=>{
-//   console.log(data);
-//   // console.log(data.username);
-//   return data.username;
-// })
-// .then((username)=>{
-//   console.log(username);
-// })
-// .catch((message)=>{
-//   console.log(message);
-// }).finally(()=> console.log(`Promise is either resolved or rejected.`));
+promiseThree.then((user)=>{
+  console.log("Thired promise is completed!");
+  console.log(`User id is: ${user.id} and user name is: ${user.name}`);
+});
 
 
-// const promiseFive = new Promise ((resolve , reject)=>{
-//   setTimeout(()=>{
-//     let error = true;
-//     if(!error){
-//       resolve({username: "JavaScript" , password: 123 })
-//     }
-//     else{
-//       reject("ERROR: JS went wrong!");
-//     }
-//   }, 1000)
-// })
+const promiseFour = new Promise ((resolve , reject)=>{
+  setTimeout(()=>{
+    let error = false;
+    if(!error){
+      resolve({username: "Robot" , password: 1234});
+    }
+    else{
+      reject("ERROR: something went wrong.");
+    }
+  },1000)
+})
 
-// async function consumePromiseFive(){
-//   try{
-//      const response = await promiseFive
-//   console.log(response);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
+promiseFour
+.then((data)=>{
+  console.log(data);
+  // console.log(data.username);
+  return data.username;
+})
+.then((username)=>{
+  console.log(username);
+})
+.catch((message)=>{
+  console.log(message);
+}).finally(()=> console.log(`Promise is either resolved or rejected.`));
 
-// consumePromiseFive();
+
+const promiseFive = new Promise ((resolve , reject)=>{
+  setTimeout(()=>{
+    let error = true;
+    if(!error){
+      resolve({username: "JavaScript" , password: 123 })
+    }
+    else{
+      reject("ERROR: JS went wrong!");
+    }
+  }, 1000)
+})
+
+async function consumePromiseFive(){
+  try{
+     const response = await promiseFive
+  console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+consumePromiseFive();
 
 
 
-// const prac = new Promise ((resolve , reject)=>{
-//   let err = false;
-//   if (!err){
-//     const user = {
-//       id: 101,
-//       targen: "Your_self"
-//     }
-//     resolve(user);
-//   }
-//   else{
-//     reject("something went wrong with data!");
-//   }
-// })
-// .then((response)=>{
-//   return response;
-// })
-// .then((data)=>{
-//  console.log(data.targen);
-// })
-// .catch((err)=>{
-//   console.log('ERROR: ', err);
-// });
+const prac = new Promise ((resolve , reject)=>{
+  let err = false;
+  if (!err){
+    const user = {
+      id: 101,
+      targen: "Your_self"
+    }
+    resolve(user);
+  }
+  else{
+    reject("something went wrong with data!");
+  }
+})
+.then((response)=>{
+  return response;
+})
+.then((data)=>{
+ console.log(data.targen);
+})
+.catch((err)=>{
+  console.log('ERROR: ', err);
+});
 
 
 // Promise Chaining
@@ -153,30 +153,29 @@
 // .catch(error => {
 //   console.log("Error:", error.message);
 // });
+
+
 // 😈
-fetch('https://jsonplaceholder.typicode.com/users/1')
-.then(response =>{
-  if (!response.ok){
+fetch('https://jsonplaceholder.typicode.com/users/4')
+.then((response)=>{
+  if(!response.ok){
     throw new Error (`HTTP Error: ${response.status}`);
   }
-  return response.json(); 
+  return response.json();
 })
-.then(user => {
-  console.log('User:' , user);
+.then((user)=>{
+  console.log(`User: ${user.name}`);
   return fetch(`https://jsonplaceholder.typicode.com/posts?userId=${user.id}`);
 })
-.then(response =>{
-  if (!response.ok){
-    throw new Error ('HTTP Error:' , response.status);
+.then((response)=>{
+  if(!response.ok){
+    throw new Error (`HTTP Error: ${response.status}`);
   }
-
   return response.json();
 })
-.then(posts =>{
-  console.log("First Posts:", posts[0].title);
-  return fetch(
-    `https://jsonplaceholder.typicode.com/comments?postId=${posts[0].id}`
-  );
+.then((post)=>{
+  console.log("Post: ",post[1].title);
+  return fetch(`https://jsonplaceholder.typicode.com/comments?postId=${post[1].id}`);
 })
 .then(response =>{
   if (!response.ok){
@@ -184,14 +183,18 @@ fetch('https://jsonplaceholder.typicode.com/users/1')
   }
   return response.json();
 })
-.then(comments => {
-  console.log("Fist Comment:", comments[0].name);
+.then(comment=>{
+  // console.log(`First comment: ${comment[0].name}`);
+  comment.forEach((element , index) => {
+    console.log(`Comment ${index+1}: ${element.name}`);
+  });
 })
-.catch(err =>{
-  console.log(`Error: ${err.message}`);
-});
-
+.catch((err)=>{
+  console.log("Error: ", err);
+})
 // 😈
+
+
 // promise nesting
 // getUser()
 //     .then(user => {
@@ -209,3 +212,5 @@ fetch('https://jsonplaceholder.typicode.com/users/1')
 //             });
 
 //     });
+
+// 🥸
