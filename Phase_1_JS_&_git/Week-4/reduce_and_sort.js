0f3cbacd3f8115aea39d061fdb8df48258bb466a
Saifullah Khalid <saifullahkhalid521@gmail.com>
2026-09-15 09:17:00 +0530

    reduce() method concepts

--- Phase_1_JS_&_git/Week-4/reduce_and_sort.js
+++ Phase_1_JS_&_git/Week-4/reduce_and_sort.js
@@ -1,3 +1,28 @@
+const fruits = ["apple", "banana", "apple", "cherry", "banana", "apple"];
+
+const count = fruits.reduce((acc, fruit) => {
+  acc[fruit] = (acc[fruit] || 0)+1;
+  return acc;
+}, {});
+
+console.log(count);
+
+
+const users = [
+  {name: "Alice" ,role: "admin"},
+  {name: "Bob" ,role: "user"},
+  {name: "Charlie" ,role: "admin"},
+  {name: "Dave" ,role: "user"}
+]
+const groupd = users.reduce((acc , user) => {
+  if (!acc[user.role]){
+    acc[user.role] = [];
+  }
+  acc[user.role].push(user);
+  return acc;
+}, {});
+console.log(groupd);
+
 console.log('Reduce -> Level 1 😊')
 const nums = [1 ,2 ,3 ,4 ,5];
 const sumOfnums = nums.reduce((acc , cVal) => {
@@ -36,3 +61,23 @@ const wordsJoin = words.reduce((acc , w)=> {
   return acc?acc + " " + w:w ;
 },"");
 console.log(wordsJoin);
+
+//Level -> 2
+console.log("Level -> 2😎");
+
+const cart = [
+  {item: "Book" , price: 15},
+  {item: "Pen" , price: 2},
+  {item: "Laptop" , price: 999}
+]
+const cartPrice = cart.reduce((acc , n) =>{
+  return acc + n.price;
+},0);
+console.log(cartPrice);
+
+const letters =["a" ,"b" ,"a" ,"c" ,"b" ,"a"];
+const lettersCount = letters.reduce((acc , n) => {
+  acc[n] = (acc[n] || 0)+1;
+  return acc;
+},{})
+console.log(lettersCount);
