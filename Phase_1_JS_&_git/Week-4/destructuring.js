383564e62c13624832295555177c326f41f23b40
Saifullah Khalid <saifullahkhalid521@gmail.com>
2026-09-18 06:41:49 +0530

    destruct practice

--- Phase_1_JS_&_git/Week-4/destructuring.js
+++ Phase_1_JS_&_git/Week-4/destructuring.js
@@ -147,3 +147,79 @@ function handleResponse({ statusCode: code, data }) {
 }
 console.log(handleResponse(response));
 
+//practice questions
+const point1 = [10 , 20];
+const [p1 , p2] = point1;
+console.log(p1 , p2);
+
+const us1 = {us1name: "Khalid" , us1age: 23};
+const {us1name , us1age} = us1;
+console.log(us1name , us1age);
+
+const arr1 = [1 , 2 , 3 , 4 , 5];
+const [ , , thd] = arr1;
+console.log(thd);
+
+const us2 = {us2name: "Bob"};
+const {us2name , us2age = 18 } = us2;
+console.log(us2name , us2age);
+
+let a3 = 1;
+let b3 = 2;
+[a3 , b3] = [b3 , a3];
+console.log(a3 , b3);
+
+const us3 = {us3name: "Saif" , us3age: 25};
+const {us3name: us3UserName , us3age } = us3;
+console.log(us3UserName , us3age);
+
+const us4 = {
+  us4name: "Robot",
+  address: {us4city: "Ranchi" ,us4zip: "834001"},
+}
+const {us4name , address:{us4city , us4zip}} = us4
+console.log(us4city , us4zip);
+
+const nums2 = [1 , 2 , 3 , 4 , 5];
+const [n2First , ...n2Rest] = nums2;
+console.log(n2First);
+console.log(...n2Rest);
+
+const us5 = {us5name: "Ego" , us5age:30};
+function introduce2({us5name , us5age}){
+  return `${us5name} is ${us5age}`;
+}
+console.log(introduce2(us5));
+
+const us6 = {
+  us6name : "Saifullah",
+  age : 22,
+  city : "Ranchi",
+  role : "Devloper",
+}
+const {us6name , ...us6rest} = us6;
+console.log(us6rest);
+
+const nuMinMax = [3 , 1 , 4 , 5 , 2]
+function getMinMax2 (nums){
+  return [Math.min(...nums) , Math.max(...nums)];
+}
+// console.log(getMinMax2(nuMinMax));
+const [min2 , max2] = getMinMax2(nuMinMax);
+console.log(min2 , max2);
+
+const state2 = ["active" , () => console.log("setter")];
+const [status2 , setStatus2] = state2;
+console.log(status2 , setStatus2);
+
+function greet3({nameG3 = 'Guest' , cityG3 = "Nowhere"} = {}){  //Defaults
+  return `Hello ${nameG3} from ${cityG3}`
+};
+console.log(greet3());
+console.log (greet3({nameG3 : "Mr.Robot"}));
+
+const response2 = {statusCode2: 200 , data: {nameRE2: "Saif"}};
+function handleResponse2({statusCode2 : Code , data}){
+  return `${Code}: ${data.nameRE2}`;
+}
+console.log(handleResponse2(response2));
